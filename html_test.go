efdfb6048c684d91685625c06fc2ace8c04049a7
Mind.R. <3825716+mindaugasrukas@users.noreply.github.com>
2024-11-30 17:25:42 -0800

    Update html_test.go

--- html_test.go
+++ html_test.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/davidscottmills/goeditorjs"
+	"github.com/StencilFrame/goeditorjs"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
